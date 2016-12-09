7fff88ddd9b2fa0d7853005260d03c0990d0f6af
Jean-Daniel Bussy <jeandaniel.bussy@stacktical.com>
2016-12-10 00:13:23 +0900

    wip usage of promises

--- src/components/stacktical-bench.js
+++ src/components/stacktical-bench.js
@@ -1,10 +1,12 @@
 /***************
 * Dependencies *
 ***************/
-var request = require('request');
+
+var Promise = require("bluebird");
+var request = Promise.promisify(require('request'));
 var lodash = require('lodash');
 
-config = require(__base +'config/config.js')();
+var config = require(__base +'config/config.js')();
 
 // Set apiKey and appId paramaters
 if (process.argv[2] && process.argv[3]) {
@@ -27,119 +29,186 @@ var request = request.defaults({
   }
 })
 
-var params = [];
+var params = {};
 var bench = {};
 var app = {};
 var test = {};
+var loadResults = {'points' : []};
 
 // Create a new test
-bench.createTest = function(apiKey, callback) {
-    console.log('Initiating new Test')
-    request.post({url: 'tests', json: true}, function(error, response, body) {
-        if (config.debug === true) {
-            console.log(JSON.stringify(body));
-        }
-        if (error) {
-            console.error(error);
-        } else {
-            test.id = JSON.stringify(body).testId;
-            console.log("Created new test...");
-            callback(null, callback)
-        }
+bench.createTest = function(apiKey) {
+    return new Promise(function(resolve, reject) {
+        console.log('Initiating new Test')
+        request.post({url: 'tests', json: true}, function(error, response, body) {
+            if (config.debug === true) {
+                console.log(JSON.stringify(body));
+            }
+            if (error) {
+                console.error(error);
+                reject({
+                    err: error
+                });
+            } else {
+                test.id = JSON.stringify(body).testId;
+                console.log("Created new test...");
+                resolve(test);
+                //callback(null, callback)
+            }
+        })
     })
 }
 
 // Fetch load test parameters from stacktical
 bench.getParams = function(apiKey, callback) {
-    console.log('Initiating getParams')
-	request.get({url: '/tests/parameters'}, function(error, response, body) {
-	  if (config.debug === true) {
-	    console.log(body);
-	  }
-	  if (error) {
-	    console.error(error);
-	  } else {
-	    // Parse the load test parameters
-	    params = JSON.stringify(body);
-	    console.log("Received parameters for app: " + app.name);
-	    //callback(null, app, params);
-	  }
-	})
+
+    return new Promise(function(resolve, reject) {
+        console.log('Initiating getParams')
+        request.get({url: '/tests/parameters'}, function(error, response, body) {
+            if (config.debug === true) {
+              console.log(body);
+            };
+
+            if (error) {
+              console.error(error);
+              reject({
+                  err: error
+              });
+            } else {
+              // Parse the load test parameters
+              //params = JSON.stringify(body);
+              params = body;
+              console.log("Received parameters for app: " + app.appId);
+              resolve(params);
+            };
+        });
+    });
 }
 
 // Submit a single load test result
 // TODO Adjust according th the API specs
 bench.loadSubmit = function(test, loadResult) {
-	request.post({url: '/tests/' + test.id, body: loadResult, json: true}, function(error, response, body) {
-	if (config.debug === true) {
-		console.log(JSON.stringify(body));
-	}
-	if (error) {
-		console.error(error);
-	} else {
-		console.log("Successfully submitted the test result unit to stacktical.");
-	}
-	})
+
+    return new Promise(function(resolve, reject) {
+        request.post({url: '/tests/' + test.id, body: loadResult, json: true}, function(error, response, body) {
+        if (config.debug === true) {
+            console.log(JSON.stringify(body));
+        };
+        if (error) {
+            console.error(error);
+            reject({
+                err: error
+            });
+        } else {
+            console.log("Successfully submitted the test result unit to stacktical.");
+        };
+        });
+    });
 }
 
 
 // Submit the load result
 // USE report stalability by using  token to verify
 bench.reportSubmit = function (loadResults) {
-  request.post({url: '/reports/scalability', body: loadResults, json: true}, function(error, response, body) {
-	if (config.debug === true) {
-		console.log(JSON.stringify(body));
-	}
-	if (error) {
-		console.error(error);
-	} else {
-		console.log("Successfully sumitted load test results to stacktical. Exiting...");
-	}
-	})
+
+    return new Promise(function(resolve, reject) {
+        request.post({url: '/reports/scalability', body: loadResults, json: true}, function(error, response, body) {
+            if (config.debug === true) {
+                console.log(JSON.stringify(body));
+            };
+            if (error) {
+                console.error(error);
+                reject({
+                    err: error
+                });
+            } else {
+                console.log("Successfully sumitted load test results to stacktical. Exiting...");
+            };
+        });
+    });
 }
 
-bench.getThroughput = function (err, endpoint, concurrency, time) {
+bench.getThroughput = function (endpoint, concurrency, time) {
     console.log("Start Load testing...");
-    var result;
 
-    var spawn = require('child_process');
+    return new Promise(function(resolve, reject) {
+        var result;
 
-    var ti = time || 60
-    var loadTest = spawn.spawnSync('siege',
-    [
-        "-t" + ti + "s",
-        "-c"+ concurrency,
-        "-b",
-        endpoint
-    ]);
+        var spawn = require('child_process');
 
-    // For some reason, the transaction rate is part of stderr, not stdout
-    result = loadTest.stderr.toString()
+        var ti = time || 60
+        var loadTest = spawn.spawnSync('siege',
+        [
+            "-t" + ti + "s",
+            "-c"+ concurrency,
+            "-b",
+            endpoint
+        ]);
+
+        // For some reason, the transaction rate is part of stderr, not stdout
+        result = loadTest.stderr.toString()
 
 
-        var bufferResult = result.split('\n');
+            var bufferResult = result.split('\n');
 
-        var validSiegeMetrics = ["Concurrency","Transaction rate"];
+            var validSiegeMetrics = ["Concurrency","Transaction rate"];
 
-        var bufferResult = bufferResult.filter(function(std) {
-            return validSiegeMetrics.some(function(metric) { return std.indexOf(metric) > -1 });
-        });
+            var bufferResult = bufferResult.filter(function(std) {
+                return validSiegeMetrics.some(function(metric) { return std.indexOf(metric) > -1 });
+            });
 
-        for (var i = bufferResult.length - 1; i >= 0; i--) {
-          bufferResult[i] = bufferResult[i].split(':');
-          bufferResult[i][0] = lodash.trim(bufferResult[i][0]);
-          bufferResult[i][1] = lodash.trim(bufferResult[i][1].split('trans/sec').join(''));
-        }
+            for (var i = bufferResult.length - 1; i >= 0; i--) {
+              bufferResult[i] = bufferResult[i].split(':');
+              bufferResult[i][0] = lodash.trim(bufferResult[i][0]);
+              bufferResult[i][1] = lodash.trim(bufferResult[i][1].split('trans/sec').join(''));
+            };
 
-        if (!result) {
-            console.error(error);
-            return false;
+            if (!result) {
+                console.error(error);
+                reject({
+                    err: error
+                });
 
-        } else {
-            console.log("Successfully ran the siege load test");
-            console.log(bufferResult);
-            return bufferResult;
-        }
+            } else {
+                console.log("Successfully ran the siege load test");
+                console.log(bufferResult);
+                resolve(bufferResult);
+            };
+    });
+}
+
+bench.iterateload = function(app, params) {
+    // TODO get the api to return the full application object
+    console.log("Starting load testing loop");
+    var params = {"parameters":[{"concurrency":20},{"concurrency":15}]};
+    console.log(params);
+    return new Promise(function(resolve, reject) {
+        var timeoutObject = setTimeout(function() {
+            for (var i in params.parameters) {
+                console.log(params.parameters[i]);
+                var concurrency = params.parameters[i].concurrency;
+                console.log(concurrency);
+                console.log(params.parameters[i]);
+                bench.getThroughput('https://localhost:10006', concurrency, 15)
+                    .then(function(ldresults) {
+                        console.log("getThroughput");
+                        var p = parseInt(ldresults[0][1]);
+                        var Xp = parseInt(ldresults[1][1]);
+                        loadResults.points.push({ 'p': p, 'Xp': Xp });
+                        submit = {"points":[{"p":5,"Xp":27.09},{"p":10,"Xp":43.41},{"p":15,"Xp":55},{"p":20,"Xp":62.06},{"p":25,"Xp":69.3},{"p":30,"Xp":74.63},{"p":35,"Xp":78.07},{"p":40,"Xp":80.05}]};
+                        bench.loadSubmit(p, Xp);
+                        for (var loadTest in app) {
+                            iterateload(null,loadTest);
+                        }
+                        console.log(submit);
+                        //bench.reportSubmit(submit).catch(function(reason) {
+                        //    console.log('Unable to submit this load test results :' + reason);
+                        //});
+                    });
+            }
+            clearTimeout(timeoutObject);
+        },2000);
+    resolve();
+    });
 };
 
 module.exports = bench;
--- src/stacktical.js
+++ src/stacktical.js
@@ -1,5 +1,6 @@
 global.__base = __dirname + '/';
 config = require(__base + 'config/config.js')();
+var Promise = require("bluebird");
 var bench = require(__base + 'components/stacktical-bench.js');
 
 /*
@@ -9,37 +10,22 @@ var bench = require(__base + 'components/stacktical-bench.js');
  * 4 [POST] /reports/scalability format and submit the data for a scalability report
  */
 
-var loadResults = {'points' : []};
-function iterateload(err, app, params) {
-	console.log(params);
-	// TODO loop through params
-	for (var i = 0, len = param.length; i < len; i++) {
-		var concurrency = app.params[i].concurrency + 20;
-		var timeoutObject = setTimeout(function() {
-				var ldresults = bench.getThroughput(null, app);
-				var p = parseInt(ldresults[0][1]);
-				var Xp = parseInt(ldresults[1][1]);
-				loadResults.points.push({ 'p': p, 'Xp': Xp });
-				/* if (app.params.concurrency >= 20) {
-					clearTimeout(timeoutObject);
-					submit = {"points":[{"p":5,"Xp":27.09},{"p":10,"Xp":43.41},{"p":15,"Xp":55},{"p":20,"Xp":62.06},{"p":25,"Xp":69.3},{"p":30,"Xp":74.63},{"p":35,"Xp":78.07},{"p":40,"Xp":80.05}]};
-					bench.submit(submit);
-				} else {
-					iterateload(null,app);
-				}*/
-			// TODO Submit the single load test result
-				// This operation is not critical
-				bench.loadSubmit(p, Xp);
-				for (var loadTest in app) {
-					iterateload(null,loadTest);
-				}
-				clearTimeout(timeoutObject);
-				bench.reportSubmit(submit);
-			}
-			, 2000
-		);
-        };
-}
 
 // Start
-bench.createTest(null, bench.getParams);
+function stacktical() {
+    bench.createTest()
+    .then(function() {
+        bench.getParams()
+        .then(function(params) {
+            console.log(params);
+            //bench.iterateload(null,null,params.parameters);
+            bench.iterateload();
+        });
+
+    })
+    .then(function() {
+        console.log("submit");
+    });
+};
+
+stacktical();
